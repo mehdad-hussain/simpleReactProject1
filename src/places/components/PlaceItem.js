ba7bb6ab66502d8f72dbbcd6358a6b4cc10cd769
mehdad-hussain <hussain.mehdad1@gmail.com>
2021-07-06 01:03:55 +0600

    3rd Commit

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,26 +1,30 @@
-import Button from "../../shared/components/UIElements/Button";
+import Button from "../../shared/components/FormElements/Button";
 
 const PlaceItem = (props) => {
   return (
-    <li style={{ listStyle: "none", margin: 6 }} className='shadow'>
+    <li className='shadow mt-4'>
       <div className='card'>
-        <img src={props.image} alt={props.title} className='img-fluid' />
+        <img
+          src={props.image}
+          alt={props.title}
+          className='w-100 h-100 img-fluid'
+        />
       </div>
-      <div className='card-body bg-purple-500'>
+      <div className=' text-center card-body  p-4'>
         <h4 className='card-title'>{props.title}</h4>
         <h5>{props.address}</h5>
         <p className='card-text'>{props.description}</p>
-        <div className=''>
-          <Button color='primary' m='-2' p='-2'>
-            View On Map
-          </Button>
-          <Button color='success' m='-2' p='x-2'>
-            Edit
-          </Button>
-          <Button color='danger' m='-2' p='-2'>
-            Delete
-          </Button>
-        </div>
+      </div>
+      <div className='p-4 text-center'>
+        <Button color='outline-primary' m='2' p='2'>
+          View On Map
+        </Button>
+        <Button color='success' m='2' py='2' px='4'>
+          Edit
+        </Button>
+        <Button color='danger' m='2' py='2' px='3' rounded='0' shadow>
+          Delete
+        </Button>
       </div>
     </li>
   );
